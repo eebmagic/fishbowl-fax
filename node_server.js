fbb90ff83a76261c7f52d751395ebf541ee2dfb6
E Bolton <eebmagic@gmail.com>
2024-01-10 18:58:24 -0500

    add func to upload docs

--- node_server.js
+++ node_server.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const fs = require('fs');
 const https = require('https');
 const path = require('path');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
@@ -14,9 +16,37 @@ const options = {
   ca: fs.readFileSync(path.join(__dirname, 'ssl/fishbowl_lol.ca-bundle')),
 }
 
+
+async function addDocumentToCollection(message) {
+  const dbURL = process.env.MONGO_URI;
+  const client = new MongoClient(dbURL);
+
+  try {
+    await client.connect();
+    console.log("Connected to server");
+
+    const db = client.db('fishbowl-fax');
+    const collection = db.collection('messages');
+
+    const doc = {
+      'message': message,
+      'date-received': new Date(),
+      'printed': false,
+      'date-printed': null
+    }
+    const result = await collection.insertOne();
+    console.log(`Added doc: ${JSON.stringify(doc, null, 2)}`);
+    // return true;
+  } catch {
+    console.log(`FAILED`);
+    // return false;
+  }
+}
+
 app.post('/addDocument', async (req, res) => {
   const { message } = req.body;
   console.log(`Got call to add doc for message: ${message}`);
+  addDocumentToCollection(message);
 });
 
 app.get('/heartbeat', async (req, res) => {
